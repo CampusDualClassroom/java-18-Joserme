235eb25b94a713c860e5ed1deff44a1dd8dd8f89
Joserme <jose.mendez@campusdual.com>
2024-09-27 19:31:54 +0200

    Ejercicio18 testok

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,47 @@
 package com.campusdual.classroom;
 
-public class Exercise18 {
+import com.campusdual.util.Utils;
+import org.w3c.dom.ls.LSOutput;
 
+import java.util.Scanner;
+
+public class Exercise18 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static int[] createAndInitializeArray(int num){
+		int[] array = new int[num];
+		for (int i = 0; i < num; i++) {
+			array[i] = i+1;
+		}
+		return array;
+	}
+	public static void showInlineArray(int[] array){
+		String valor ="";
+		String a="";
+		String salida = "";
+		for (int i = 0; i < array.length; i++) {
+			int num = array[i];
+			salida += String.valueOf(num);
+			if(i < array.length-1){
+				a= " ";
+				salida +=  a;
+			}
+
+
+			
+		}System.out.print(salida);
+	}
+	public static int solicitarDatos(){
+		//System.out.println("Escriba la cantidad de elementos");
+		//String peticion = sc.nextInt();
+		int dato = Utils.integer("introduce numero");
+		return dato;
+	}
 	public static void main(String[] args) {
+			int numero = solicitarDatos();
+
+		int[] array = createAndInitializeArray(numero);
+			showInlineArray(array);
 
 	}
 }
